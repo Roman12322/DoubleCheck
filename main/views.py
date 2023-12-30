b238b9f18b9bba85f27c1699f3a80ef8aa236176
Roman Heen <mega.fo@list.ru>
2023-12-30 15:07:45 +0300

    refactord code 30.12.23

--- main/views.py
+++ main/views.py
@@ -4,11 +4,23 @@ from .model import pipeline, Meso4
 # Create your views here.
 
 def render_main_window(request):
-    return render(request, "main_window.html")
+    return render(request, "main/index.html")
+
+def render_about_window(request):
+    return render(request, "main/about.html")
+
+def render_service_window(request):
+    return render(request, "main/service.html")
+
+def render_team_window(request):
+    return render(request, "main/team.html")
+
+def render_why_window(request):
+    return render(request, "main/why.html")    
 
 def recognize_person(request):
     avg_score = pipeline()
     context = {'avg_score': avg_score}
-    return render(request, 'main_window.html', context)
+    return render(request, 'main/main_window.html', context)
  
 
