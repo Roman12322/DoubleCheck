5c9104d33ce09c2464e8c26a54e6f7fc809da1af
Roman Heen <mega.fo@list.ru>
2024-01-03 19:40:48 +0300

    added CSRF trusted origin url

--- doublecheck/settings.py
+++ doublecheck/settings.py
@@ -126,6 +126,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
 SESSION_COOKIE_AGE = 60 * 60 * 24 * 7  # одна неделя
+CSRF_TRUSTED_ORIGINS = [
+    'https://doublecheck-production.up.railway.app/'
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
