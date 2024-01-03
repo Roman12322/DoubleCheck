3738077ac44e4432f27336df402b33a1317352b7
Roman Heen <mega.fo@list.ru>
2024-01-03 19:54:19 +0300

    added CSRF trusted origin url

--- main/models.py
+++ main/models.py
@@ -1,11 +1,11 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import AbstractBaseUser, UserManager
 from django.core.validators import MinLengthValidator
 
 class User(AbstractBaseUser):
     email = models.EmailField(verbose_name='email address', max_length=255, unique=True)
     password = models.CharField(max_length=128, validators=[MinLengthValidator(8)])
-
+    objects = UserManager()
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
 
