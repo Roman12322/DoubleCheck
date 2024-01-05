c470b26e99ddc5490ac2a9d21c5af8daf5dbed4e
Roman Heen <mega.fo@list.ru>
2024-01-05 15:51:20 +0300

    added NGINX proxy to handle static files

--- doublecheck/settings.py
+++ doublecheck/settings.py
@@ -122,13 +122,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = '/static/doublecheck'
+STATIC_URL = '/doublecheck/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_DIRS = []
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/doublecheck'
+MEDIA_URL = '/doublecheck/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
