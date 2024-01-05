7c02a3622be8a2ffc583f8d977edb9793cf6ba67
Roman Heen <mega.fo@list.ru>
2024-01-05 14:06:56 +0300

    fixing error with new files v2

--- main/static/main/js/video-recording.js
+++ main/static/main/js/video-recording.js
@@ -32,6 +32,7 @@ window.onload = function () {
               console.log(recordedBlobs)
               console.log(blob)
 
+              
               $.ajax({
                 method: "POST",
                 url: "/recognize_person",
