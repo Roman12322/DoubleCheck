0c9e18ee7742f00417a292986be9398f9af5a39e
Roman Heen <mega.fo@list.ru>
2024-01-07 00:15:23 +0300

    change back to submit hander

--- static/main/js/video-recording.js
+++ static/main/js/video-recording.js
@@ -15,8 +15,7 @@ window.onload = function () {
         video.srcObject = stream;
         // record-button
         recordButton.onclick = function () {
-            var options = {mimeType: "video/webm"}
-            mediaRecorder = new MediaRecorder(stream, options);
+            mediaRecorder = new MediaRecorder(stream);
             mediaRecorder.start(100);   
             mediaRecorder.ondataavailable = (event) => {
                 if (event.data && event.data.size > 0) {
