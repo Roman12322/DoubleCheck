7e76742365e7d18370a871f2f94faebbd3620fc7
Roman Heen <mega.fo@list.ru>
2024-01-07 18:43:35 +0300

    fixing js file v4

--- static/main/js/video-recording.js
+++ static/main/js/video-recording.js
@@ -2,7 +2,7 @@ function getRandomInt(max) {
   return Math.floor(Math.random() * max).toString();
 }
 
-window.onload = function () {
+window.onload = async function () {
     var video = document.querySelector('video');
     const recordButton = document.querySelector('button#record');
     const stopButton = document.querySelector('button#stop');
@@ -10,61 +10,61 @@ window.onload = function () {
     const csrftoken = document.querySelector('[name=csrfmiddlewaretoken]').value;
     const max_v = 1234567890
 
-    var stream = navigator.mediaDevices.getUserMedia({video: true});
-    video.srcObject = stream;
-    // record-button
-    mediaRecorder = new MediaRecorder(stream);
-    recordButton.onclick = function () {
-        mediaRecorder.start(100);   
-        mediaRecorder.ondataavailable = (event) => {
-            if (event.data && event.data.size > 0) {
-            recordedBlobs.push(event.data);
-            };}
-    }
+    const mediaStream = await navigator.mediaDevices.getUserMedia({ video: true });
+    video.srcObject = mediaStream;
+        // record-button
+        recordButton.onclick = function () {
+            mediaRecorder = new MediaRecorder(mediaStream);
+            mediaRecorder.start(100);   
+            mediaRecorder.ondataavailable = (event) => {
+                if (event.data && event.data.size > 0) {
+                recordedBlobs.push(event.data);
+                };}
+        }
 
-    $("form").submit(function (event) {
-      try {
-        event.preventDefault();
-        mediaRecorder.stop();
-        if (recordedBlobs.length >2) {
-          val = getRandomInt(max_v);
-          filename = 'video'.concat(val);
+        $("form").submit(function (event) {
+          try {
+            event.preventDefault();
+            mediaRecorder.stop();
+            if (recordedBlobs.length >2) {
+              val = getRandomInt(max_v);
+              filename = 'video'.concat(val);
 
-          let blob = new Blob(recordedBlobs, {type: 'video/webm'});
-          let formdata = new FormData();
-          formdata.append("filename", filename.concat('.webm'));
-          formdata.append(filename, blob);
-          
-          // logging
-          console.log(recordedBlobs)
-          console.log(blob)
-          console.log(filename)
+              let blob = new Blob(recordedBlobs, {type: 'video/webm'});
+              let formdata = new FormData();
+              formdata.append("filename", filename.concat('.webm'));
+              formdata.append(filename, blob);
+              
+              // logging
+              console.log(recordedBlobs)
+              console.log(blob)
+              console.log(filename)
 
-          // ajax request
-          $.ajax({
-            method: "POST",
-            url: "/recognize_person",
-            data: formdata,
-            processData: false,
-            contentType: false,
-            success: function(response) {
-              console.log("response executed successfully", response)
-            },
-            error: function(response) {
-              console.log("response executed with error", response)
+              // ajax request
+              $.ajax({
+                method: "POST",
+                url: "/recognize_person",
+                data: formdata,
+                processData: false,
+                contentType: false,
+                success: function(response) {
+                  console.log("response executed successfully", response)
+                },
+                error: function(response) {
+                  console.log("response executed with error", response)
+                }
+              }).done(function(response) {
+                alert(response);
+              });
+              formdata = null;
+              recordedBlobs = [];
             }
-          }).done(function(response) {
-            alert(response);
-          });
-          formdata = null;
-          recordedBlobs = [];
-        }
-        else {
-          alert("Record haven't found or video is too short! Try again");
-        }
+            else {
+              alert("Record haven't found or video is too short! Try again");
+            }
 
-      } catch (error) {
-        alert("Record haven't found, please try again!", error);
-      }
-      });
+          } catch (error) {
+            alert("Record haven't found, please try again!", error);
+          }
+          });
 }
