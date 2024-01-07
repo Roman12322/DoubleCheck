3169751a00ae8748a1369437109e70648f263ab7
Roman Heen <mega.fo@list.ru>
2024-01-07 15:31:02 +0300

    fixing iphone video stream bug v1

--- main/model.py
+++ main/model.py
@@ -16,8 +16,11 @@ SAVING_FRAMES_PER_SECOND = 1
 
 def preprocessing_video(video_file):
     # load video-file
-    readed_file = np.frombuffer(video_file)
-    print(f"numpy read from buffer: {readed_file} | shape {readed_file.shape}")
+    try:
+        readed_file = (video_file.read())
+        print(f"readed file: {readed_file} |")
+    except Exception as e:
+        print(e)
     frames = iio.imread(video_file, index=None, format_hint='.webm')
     print(f"frames' shape: {frames.shape}")
     return frames
